1769fbcc8b16ae4271466869b182ab109ca557b0
Anshuman Rahul <anshumanrahul06@gmail.com>
2022-05-20 16:45:21 +0530

    The stlying implementation is most likely shit but fuck off, it works(somehow)

--- client/src/AlbumList/AlbumList.js
+++ client/src/AlbumList/AlbumList.js
@@ -1,26 +1,26 @@
-import React from "react"
-import "./albumlist.css"
-import Album from "../Album/Album"
+import React from "react";
+import "./albumlist.css";
+import Album from "../Album/Album";
 
 const convertToDecades = (albumdata) => {
-  let decadesData = {}
-  Object.keys(albumdata).forEach((year) => {
-    if (decadesData[year.slice(0, 3) + "0s"]) {
-      decadesData[year.slice(0, 3) + "0s"].push(...albumdata[year])
-    } else {
-      decadesData[year.slice(0, 3) + "0s"] = [...albumdata[year]]
-    }
-  })
-  return decadesData
-}
+    let decadesData = {};
+    Object.keys(albumdata).forEach((year) => {
+        if (decadesData[year.slice(0, 3) + "0s"]) {
+            decadesData[year.slice(0, 3) + "0s"].push(...albumdata[year]);
+        } else {
+            decadesData[year.slice(0, 3) + "0s"] = [...albumdata[year]];
+        }
+    });
+    return decadesData;
+};
 
 const AlbumList = ({ data, toShowYear }) => {
-  if (!toShowYear) {
-    data = convertToDecades(data)
-  }
-  return (
-    <div id="albums">
-      {/* <div id="navigation">
+    if (!toShowYear) {
+        data = convertToDecades(data);
+    }
+    return (
+        <div id="albums">
+            {/* <div id="navigation">
         <div>
           <ul>
             {Object.keys(data).map((year) => (
@@ -31,30 +31,30 @@ const AlbumList = ({ data, toShowYear }) => {
           </ul>
         </div>
        </div> */}
-      <div>
-        {Object.keys(data).map((timeFrame) => (
-          <div id={timeFrame}>
-            <p className="time-frame">
-              {timeFrame} ({data[timeFrame].length})
-            </p>
-            <div className="timeframe-albums">
-              {data[timeFrame].map((album) => (
-                <Album
-                  name={album.name}
-                  artist={album.artist}
-                  cover={album.cover}
-                  preview={album.preview}
-                  dateAdded={album.dateAdded}
-                  previewTrackName={album.previewTrackName}
-                  key={`${album.name} - ${album.dateAdded}`}
-                />
-              ))}
+            <div>
+                {Object.keys(data).map((timeFrame) => (
+                    <div id={timeFrame} key={timeFrame}>
+                        <p className="time-frame">
+                            {timeFrame} ({data[timeFrame].length})
+                        </p>
+                        <div className="timeframe-albums">
+                            {data[timeFrame].map((album) => (
+                                <Album
+                                    name={album.name}
+                                    artist={album.artist}
+                                    cover={album.cover}
+                                    preview={album.preview}
+                                    dateAdded={album.dateAdded}
+                                    previewTrackName={album.previewTrackName}
+                                    key={`${album.artist} - ${album.name} - ${album.dateAdded}`}
+                                />
+                            ))}
+                        </div>
+                    </div>
+                ))}
             </div>
-          </div>
-        ))}
-      </div>
-    </div>
-  )
-}
+        </div>
+    );
+};
 
-export default AlbumList
+export default AlbumList;
--- client/src/App.js
+++ client/src/App.js
@@ -1,70 +1,50 @@
-import Dashboard from "./Dashboard/Dashboard"
-import Login from "./Login/Login"
-import "./App.css"
-import axios from "axios"
+import Dashboard from "./Dashboard/Dashboard";
+import Login from "./Login/Login";
+import "./App.css";
+import axios from "axios";
 
 function refreshAccessToke() {
-  axios
-    .post(process.env.REACT_APP_SERVER + "/refresh", {
-      refreshToken: localStorage.getItem("refreshToken"),
-    })
-    .then((data) => {
-      localStorage.setItem("accessToken", data.data.accessToken)
-      localStorage.setItem("expiresIn", data.data.expiresIn)
-    })
-    .catch((err) => console.log(err))
+    axios
+        .post(process.env.REACT_APP_SERVER + "/refresh", {
+            refreshToken: localStorage.getItem("refreshToken"),
+        })
+        .then((data) => {
+            localStorage.setItem("accessToken", data.data.accessToken);
+            localStorage.setItem("expiresIn", data.data.expiresIn);
+        })
+        .catch((err) => console.log(err));
 }
 
 function checkLocal() {
-  if (
-    new Date().getTime() > parseInt(localStorage.getItem("expiresIn")) &&
-    localStorage.getItem("refreshToken")
-  ) {
-    refreshAccessToke()
-    return true
-  }
-  if (
-    !localStorage.getItem("accessToken") ||
-    !localStorage.getItem("refreshToken") ||
-    !localStorage.getItem("expiresIn") ||
-    new Date().getTime() > parseInt(localStorage.getItem("expiresIn"))
-  ) {
-    return false
-  }
-  return true
+    if (new Date().getTime() > parseInt(localStorage.getItem("expiresIn")) && localStorage.getItem("refreshToken")) {
+        refreshAccessToke();
+        return true;
+    }
+    if (
+        !localStorage.getItem("accessToken") ||
+        !localStorage.getItem("refreshToken") ||
+        !localStorage.getItem("expiresIn") ||
+        new Date().getTime() > parseInt(localStorage.getItem("expiresIn"))
+    ) {
+        return false;
+    }
+    return true;
 }
 
 function checkParams() {
-  const params = new URLSearchParams(window.location.search)
-  console.log(new Date().getTime() + parseInt(params.get("expiresIn")) * 1000)
-  if (
-    params.get("accessToken") &&
-    params.get("refreshToken") &&
-    params.get("expiresIn")
-  ) {
-    localStorage.setItem("accessToken", params.get("accessToken"))
-    localStorage.setItem("refreshToken", params.get("refreshToken"))
-    localStorage.setItem(
-      "expiresIn",
-      new Date().getTime() + parseInt(params.get("expiresIn")) * 1000
-    )
-    return true
-  }
-  return false
+    const params = new URLSearchParams(window.location.search);
+    console.log(new Date().getTime() + parseInt(params.get("expiresIn")) * 1000);
+    if (params.get("accessToken") && params.get("refreshToken") && params.get("expiresIn")) {
+        localStorage.setItem("accessToken", params.get("accessToken"));
+        localStorage.setItem("refreshToken", params.get("refreshToken"));
+        localStorage.setItem("expiresIn", new Date().getTime() + parseInt(params.get("expiresIn")) * 1000);
+        return true;
+    }
+    return false;
 }
 
 function App() {
-  return (
-    <div>
-      {checkLocal() ||
-      (checkParams() &&
-        new Date().getTime() < parseInt(localStorage.getItem("expiresIn"))) ? (
-        <Dashboard />
-      ) : (
-        <Login />
-      )}
-    </div>
-  )
+    return <div>{checkLocal() || (checkParams() && new Date().getTime() < parseInt(localStorage.getItem("expiresIn"))) ? <Dashboard /> : <Login />}</div>;
 }
 
-export default App
+export default App;
